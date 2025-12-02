d38b2144262eb352e257d75b21d6a44569288a15
Abdullah Ismail <mongodbms@gmail.com>
2025-12-02 03:28:18 +0100

    perf: Add manual chunking configuration to split node_modules into separate bundles.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,22 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split node_modules into separate chunks by package name
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       "/mixpanel": {
